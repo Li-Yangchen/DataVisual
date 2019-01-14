27bafe14190035122c3eb59cc205d2de80ef2173
Li-Yangchen <lyc19920828@126.com>
2019-01-14 18:03:33 +0800

    20190114

--- scatter_squares.py
+++ scatter_squares.py
@@ -2,11 +2,14 @@
 # -*- coding: UTF-8 -*-
 
 import matplotlib.pyplot as plt
-squares = [1, 4, 9, 16, 25]
-input_values=[1,2,3,4,5]
-plt.plot(input_values,squares,linewidth=1)
+
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+plt.scatter(x_values, y_values, s=100)
+
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
-plt.tick_params(axis='both', labelsize=14)
+
+plt.tick_params(axis='both',which='major',labelsize=14)
 plt.show()
